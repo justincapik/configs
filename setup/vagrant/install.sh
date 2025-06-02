7a3141e109761315c617664a11abd7f9c263d437
Justin CAPIK <jucapik@f2r10s4.clusters.42paris.fr>
2025-06-02 14:46:00 +0200

    updated vagant file for Kkalilinux GPG key problem

--- setup/vagrant/install.sh
+++ setup/vagrant/install.sh
@@ -1,15 +1,27 @@
 #!/usr/bin/env bash
 set -e  # Exit on most errors by default
 
+sudo wget https://archive.kali.org/archive-keyring.gpg -O /usr/share/keyrings/kali-archive-keyring.gpg
+
 # Set Paris time
 echo "Europe/Paris" > /etc/timezone
 ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
 dpkg-reconfigure -f noninteractive tzdata
 
 # Update package lists
-sudo apt-get update
+echo "INSTALLING UPDATES"
+echo "INSTALLING autoremove"
+sudo apt autoremove -y
+echo "INSTALLING update"
+sudo apt-get update -y
+#echo "INSTALLING autoremove"
+#sudo apt upgrade -y
+echo "INSTALLING autoremove"
+sudo apt autoremove -y
+echo "FINISHED INSTALLING UPDATES"
 
 # install oh-my-zsh
+echo "Installing oh-my-zsh..."
 if [ -d /home/vagrant/.oh-my-zsh ]; then
   echo "Oh My Zsh is already installed. Skipping installation."
 else
@@ -19,14 +31,27 @@ fi
 
 # Add Docker's official GPG key:
 sudo apt-get update -y
+sudo apt-get install ca-certificates curl -y
 sudo install -m 0755 -d /etc/apt/keyrings
 sudo curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
 sudo chmod a+r /etc/apt/keyrings/docker.asc
+
 # Add the repository to Apt sources:
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian \
   $(. /etc/os-release && echo "bookworm") stable" | \
   sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update -y
+sudo apt-get install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+# give docker group auth
+#sudo usermod -aG docker $USER
+#newgrp docker
+
+# enable docker at startup
+#sudo systemctl enable docker
+#sudo systemctl start docker
 
 # List of packages you want to install
 PACKAGES=(
@@ -57,35 +82,23 @@ PACKAGES=(
 	htop
 	webp
 	apt-transport-https
-	ca-certificates
-	software-properties-common
 	gnupg
 	lsb-release
 	sudo apt update
-	docker-ce
-	docker-ce-cli
-	containerd.io
 )
 
-sudo apt update
+sudo apt update -y
 
 # Install each package individually so we can continue if one fails
 for pkg in "${PACKAGES[@]}"; do
 	echo "Attempting to install $pkg..."
 	# Use '|| true' to avoid failing the entire script if one package fails
-	sudo apt-get install -y "$pkg" || echo "WARNING: Failed to install $pkg. Continuing..."
+	sudo apt-get install -y "$pkg" || \
+		echo "WARNING: Failed to install $pkg. Continuing..."
 done
 
 
 sudo apt autoremove -y
-
-# give docker group auth
-sudo usermod -aG docker $USER
-newgrp docker
-
-# enable docker at startup
-sudo systemctl enable docker
-sudo systemctl start docker
 
 #VS Code
 
@@ -94,7 +107,7 @@ curl -sSL https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor | su
 echo "deb [arch=amd64 signed-by=/usr/share/keyrings/packages.microsoft.gpg] https://packages.microsoft.com/repos/code stable main" | sudo tee /etc/apt/sources.list.d/vscode.list
 
 # Update and install VSCode
-sudo apt-get update
+sudo apt-get update -y
 sudo apt-get install -y code
 
 # Install vscode extensions
